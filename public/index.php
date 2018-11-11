b5998e15e36fcab3e0accbd74e4dcb61aff6093d
Hayk Karapetyan <haykokalipsis@gmail.com>
2018-11-11 23:34:28 +0400

    Add dispatch method to router and a couple of string formatting methods
    Also add Post controller with a couple of methods just for tests
    
    * Changes to be committed:
    +	new file:   App/Controllers/Posts.php
    ~	modified:   Core/Router.php
    ~	modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once '../Core/Router.php';
+require_once '../App/Controllers/Posts.php';
 
 $router = new Router();
 
@@ -14,17 +15,19 @@ $router->add('admin/{controller}/{id:\d+}/{action}');
 $router->add('admin/{controller}/{name:[a-z-]+}/{action}');
 
 // Display the Routing table.
-echo "<pre>";
-print_r($router->getRoutes() );
-echo "</pre>";
+//echo "<pre>";
+//print_r($router->getRoutes() );
+//echo "</pre>";
 
 // Match the requested route
 $url = $_SERVER['QUERY_STRING'];
 
-if ($router->match($url) ) {
-    echo "<pre>";
-    print_r($router->getParams() );
-    echo "</pre>";
-} else {
-    echo "No route found for URL '$url' ";
-}
+//if ($router->match($url) ) {
+//    echo "<pre>";
+//    print_r($router->getParams() );
+//    echo "</pre>";
+//} else {
+//    echo "No route found for URL '$url' ";
+//}
+
+$router->dispatch($url);

97d5b0aaae004d164fb46caf61a3fdb8e6eef245
Hayk Karapetyan <haykokalipsis@gmail.com>
2018-11-12 21:58:53 +0400

    Create Config file in App folder, move db data there
    * Changes to be committed:
    +	new file:   App/Config.php
    .	modified:   Core/Model.php

---
+++ App/Config.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ *
+ * PHP version 5.4
+ */
+class Config
+{
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = 'test_own-mvc-udemy';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = 'root';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = '';
+}

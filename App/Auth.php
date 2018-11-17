5b3bab9768d60e1c72e81972349701f792e1a050
Hayk Karapetyan <haykokalipsis@gmail.com>
2018-11-17 15:23:12 +0400

    Implement remember_me functionality
    Create RememberedLogin class,
    log in from cookie if exists, check expiration
    delete cookie from database and browser on logout
    
    * Changes to be committed:
    .	modified:   App/Auth.php
    +	new file:   App/Models/RememberedLogin.php
    .	modified:   App/Models/User.php

--- App/Auth.php
+++ App/Auth.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\RememberedLogin;
 use App\Models\User;
 
 class Auth
@@ -12,7 +13,8 @@ class Auth
         $_SESSION['user_id'] = $user->id;
 
         if($remember_me)
-            $user->rememberLogin();
+            if($user->rememberLogin() )
+                setcookie('remember_me', $user->remember_token, $user->expiry_timestamp, '/');
     }
 
     public static function logout()
@@ -37,6 +39,7 @@ class Auth
 
         // Finally destroy the session
         session_destroy();
+        static::forgetLogin();
     }
 
 //    public static function isLoggedIn()
@@ -58,6 +61,36 @@ class Auth
     {
         if(isset($_SESSION['user_id']) ) {
             return User::findById($_SESSION['user_id']);
+        } else {
+            return static::loginFromRememberCookie();
         }
+    }
+
+    public static function loginFromRememberCookie()
+    {
+        $cookie = $_COOKIE['remember_me'] ?? false;
+
+        if($cookie) {
+            $remembered_login = RememberedLogin::findByToken($cookie);
+            if($remembered_login && ! $remembered_login->hasExpired() ) {
+                $user = $remembered_login->getUser();
+                static::login($user, false);
+                return $user;
+            }
+        }
+    }
+
+    public static function forgetLogin()
+    {
+        $cookie = $_COOKIE['remember_me'] ?? false;
+        if($cookie) {
+            $remember_login = RememberedLogin::findByToken($cookie);
+            if ($remember_login) {
+                $remember_login->delete();
+            }
+
+            setcookie('remember_me', '', time() - 3600); // set to expire in the past
+        }
+
     }
 }
